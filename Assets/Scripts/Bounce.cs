0cd397b55579b20cdb779ed397fa585f81ef771d
Sky-Pie22 <Skyler.foster@trojans.dsu.edu>
2022-09-30 14:27:29 -0500

    Add sc

---
+++ Assets/Scripts/Bounce.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+    /*public float offset, speed;
+    public AnimationCurve height, movement;
+    public Transform cubeA, cubeB;
+    private float rate;
+    private Vector3 desiredPosition;
+
+    private void Update()
+
+    {
+
+        rate += Time.deltaTime * speed;
+
+
+
+        desiredPosition = Vector3.Lerp(cubeA.position, cubeB.position, movement.Evaluate(rate));
+
+        desiredPosition.y += height.Evaluate(rate) + offset;
+
+        transform.position = desiredPosition;
+
+
+
+        if (rate >= 1)
+
+            rate = 0;
+
+    }
+
+}*/
+} 
+
