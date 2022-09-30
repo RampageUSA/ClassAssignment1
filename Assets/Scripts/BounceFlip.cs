0333f7a6c2ad622acee8e4385c4b0c6633a9e89f
Sky-Pie22 <Skyler.foster@trojans.dsu.edu>
2022-09-30 14:37:23 -0500

    Flip code

--- Assets/Scripts/BounceFlip.cs
+++ Assets/Scripts/BounceFlip.cs
@@ -3,6 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BounceFlip : MonoBehaviour
-{
+{ }
+        private bool bFlipDirection;
+        // Update is called once per frame
 
-}
+        void Update()
+
+        {
+            if (bFlipDirection)
+                transform.position += Vector3.forward * Time.deltaTime * 5;
+            else
+                transform.position += Vector3.back * Time.deltaTime * 5;
+
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (bFlipDirection) bFlipDirection = false;
+            else bFlipDirection = true;
+
+        }
+
+    }
