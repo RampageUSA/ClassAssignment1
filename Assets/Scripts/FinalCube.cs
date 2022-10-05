46a1211410fa18e9af21e19eea8bb9e8a82decbb
RampageUSA <chad.compton@trojans.dsu.edu>
2022-10-05 14:20:51 -0500

    Update FinalCube.cs

--- Assets/Scripts/FinalCube.cs
+++ Assets/Scripts/FinalCube.cs
@@ -10,7 +10,9 @@ public class FinalCube : MonoBehaviour
     private Vector3 scaleTransform;
     private float rate;
     private bool inOut;
-
+    [SerializeField]
+    [Range(1, 10)]
+    private float speed;
     //BOX.JIGGLE
     private void Start()
     {
@@ -20,26 +22,22 @@ public class FinalCube : MonoBehaviour
     {
         if (inOut)
         {
-            rate += Time.deltaTime;
+            rate += Time.deltaTime * speed;
 
             scaleTransform = new Vector3(1, height.Evaluate(rate), 1);
             transform.parent.localScale = scaleTransform;
 
             if (rate >= 1)
+            {
+                inOut = false;
                 rate = 0;
+            }
+               
         }
-        else
-        {
-            transform.parent.localScale = new Vector3(1, 1, 1);
-        }
-        
+
     }
     private void OnTriggerEnter(Collider other)
     {
         inOut = true;
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        inOut = false;
     }
 }
